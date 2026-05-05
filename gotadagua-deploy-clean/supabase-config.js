38dece8f981318a3d93d441154cab649cab9f3d1
Gotadaguasurf <accounting@gotadaguasurf.com>
2026-05-05 18:20:20 +0200

    Add files via upload

---
+++ gotadagua-deploy-clean/supabase-config.js
@@ -0,0 +1,4 @@
+window.__SUPABASE_CONFIG = {
+  url: 'https://wnksmcjqnbxaagyhfxlt.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indua3NtY2pxbmJ4YWFneWhmeGx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczNjY1MjgsImV4cCI6MjA5Mjk0MjUyOH0.qsWunzb_wo9nujawboVSd5-P-FYsEgCVqmgs9oCHq-I'
+};
